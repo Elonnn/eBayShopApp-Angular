ae36c2fb408e5513b6c390cad551751da24e53c0
Elonnn <yilangxu@usc.edu>
2020-06-12 19:13:15 -0700

    run with hello world

---
+++ src/app/item/item-card/item-card.compenent.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-card',
+  templateUrl: './item-card.component.html'
+})
+export class ItemCardComponent {
+
+}
---
+++ src/app/item/item.model.ts
@@ -0,0 +1,14 @@
+interface Item {
+  title: string,
+  condition: string,
+  category: string,
+  itemURL: string,
+  price: string,
+  shippingPrice: string,
+  location: string,
+  imageURL: string,
+
+  isReturnAccepted: boolean,
+  isTopRated: boolean,
+  isExpedited: boolean
+}
